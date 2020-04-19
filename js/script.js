07982b9086d7d627904314062122b69441a5a476
Richard Stover <stoverre@gmail.com>
2020-04-18 23:35:59 -0400

    added script.js and reset.css

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 3 - Create an Interactive Form
+Richard Stover
+4/19/2020
+******************************************/
