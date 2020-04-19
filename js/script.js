0e342704b22dc959bd7e3797bf76c004daa1fdb7
Richard Stover <stoverre@gmail.com>
2020-04-19 00:14:59 -0400

    set focus and mod job role
    1. set focus on the name field when the page loads
    2. create a text input field when 'other' is selected for job role

--- js/script.js
+++ js/script.js
@@ -4,3 +4,32 @@ FSJS project 3 - Create an Interactive Form
 Richard Stover
 4/19/2020
 ******************************************/
+
+let formElement = document.querySelector('form')
+
+//get reference to each <fieldset> (category) of the form
+let basicInfo = formElement.firstElementChild
+let shirt = basicInfo.nextElementSibling
+let activities = shirt.nextElementSibling
+let payment = activities.nextElementSibling
+
+//set focus on the Name field
+basicInfo.querySelector('input').focus()
+
+//if "other" is selected, create a new input element
+basicInfo.addEventListener('click', event => {
+    if(event.target.id === 'title'){
+        if(event.target.value === "other"){
+            let newElement = document.createElement('input')
+            newElement.type = 'text'
+            newElement.id = 'other-title'
+            newElement.placeholder = 'Your Job Role'
+            basicInfo.appendChild(newElement)
+            newElement.focus()
+        }
+    }
+})
+
+
+
+
