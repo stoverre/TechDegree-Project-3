084eec2a4792c637497ff4bd9d56b530604241f5
Richard Stover <stoverre@gmail.com>
2020-04-19 02:52:22 -0400

    shirt design and color
    fully functional shirt color selection based on the shirt design selected

--- js/script.js
+++ js/script.js
@@ -6,18 +6,63 @@ Richard Stover
 ******************************************/
 
 let formElement = document.querySelector('form')
+let pageCategoriesList = formElement.children
 
 //get reference to each <fieldset> (category) of the form
-let basicInfo = formElement.firstElementChild
-let shirt = basicInfo.nextElementSibling
-let activities = shirt.nextElementSibling
-let payment = activities.nextElementSibling
+let basicInfo = pageCategoriesList[0]
+let shirt = pageCategoriesList[1]
+let activities = pageCategoriesList[2]
+let payment = pageCategoriesList[3]
+
+//HTMLCollection of the shirt color options
+let shirtColors = shirt.querySelector('#color').children
+
+//create the color placeholder <option>
+let colorList = document.querySelector('#color')
+let colorPlaceHolder = document.createElement('option')
+colorPlaceHolder.value="placeholder"
+colorPlaceHolder.innerHTML = 'Please select a T-shirt theme'
+colorList.insertBefore(colorPlaceHolder, colorList.firstElementChild)
+
+function initializeShirtColorState(){
+    shirt.querySelector('#color').value = "placeholder"
+    //hide everything except the placeholder
+    shirtColors[0].style.display = ''
+    for(let i=1; i<shirtColors.length; i+=1){
+        shirtColors[i].style.display = 'none'
+    }
+}
+
+function updateShirtColors(event){    
+    //clear the last selection from the color field
+    shirt.querySelector('#color').value = ''
+    let regex
+    
+    if(event.target.value === "js puns"){
+        regex = /.*JS Puns.*/i
+    } else if (event.target.value === "heart js"){
+        regex = /.*JS shirt.*/i
+    } else {
+        initializeShirtColorState()
+        return
+    }
+
+    for(let i=0; i<shirtColors.length; i+=1){
+        if(regex.test(shirtColors[i].innerHTML)){
+            shirtColors[i].style.display = ''
+        } else {
+            shirtColors[i].style.display = 'none'
+        }
+    }
+}
 
 //set focus on the Name field
 basicInfo.querySelector('input').focus()
 
+initializeShirtColorState()
+
 //if "other" is selected, create a new input element
-basicInfo.addEventListener('click', event => {
+basicInfo.addEventListener('change', event => {
     if(event.target.id === 'title'){
         if(event.target.value === "other"){
             let newElement = document.createElement('input')
@@ -28,6 +73,11 @@ basicInfo.addEventListener('click', event => {
             newElement.type = 'hidden'
         }
     }
+})
+
+//showing specific shirt colors based on the design selected
+document.querySelector('#design').addEventListener('change', event => {
+    updateShirtColors(event)        
 })
 
 
