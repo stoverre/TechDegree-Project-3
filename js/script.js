b4e45763ef53e51b47201d752ef60a40f0fb401e
Richard Stover <stoverre@gmail.com>
2020-04-19 17:02:28 -0400

    credit card cvv validation

--- js/script.js
+++ js/script.js
@@ -169,26 +169,32 @@ function validateFormInputs(event){
     let email = basicInfo.children[4]
     let cost = parseInt(actCost.innerHTML.match(/[0-9]+.\d{2}/)[0])
     let ccNum = payment.querySelector('#cc-num').value
-    let ccZip = payment.querySelector('#zip')
-    let ccCVV = payment.querySelector('#cvv')
+    let ccZip = payment.querySelector('#zip').value
+    let ccCVV = payment.querySelector('#cvv').value
     
     let nameRegex = /\w+/
     let emailRegex = /^\w+@\w+\.[a-z]{3}$/i
     //2nd half of the top level or accounts for american express format 
-    let ccNumRegEx = /(^\d{4}[ -]?\d{4}[ -]?\d{4}[ -]?\d{1,4}$|^\d{4}[ -]?\d{6}[ -]?\d{5}$)/
+    let ccNumRegex = /(^\d{4}[ -]?\d{4}[ -]?\d{4}[ -]?\d{1,4}$|^\d{4}[ -]?\d{6}[ -]?\d{5}$)/
+    let ccZipRegex = /^\d{5}$/
+    let ccCVVRegex = /^\d{3}$/
 
     //if the following conditions are all met, submit the form
     //1. name is not blank
     //2. email is in the format of words@words.3(letters), submit the form
     //3. at least one activity has been selected
-    //4. credit card number is 13-16 digits (nested a for-loop to only validate
-    //   the CC # if the credit card option was selected)
+    //4. if credit card is the selected payment method
+    //   4a. number is 13-16 numerals
+    //   4b. zip is 5 numerals
+    //   4c. cvv is 3 numerals
     if(payment.children[3].style.display === ''){
         if(
             nameRegex.test(nameField.value) && 
             emailRegex.test(email.value) &&
             cost>0 &&
-            ccNumRegEx.test(ccNum)){            
+            ccNumRegex.test(ccNum) &&
+            ccZipRegex.test(ccZip) &&
+            ccCVVRegex.test(ccCVV)){            
                 submitButton.setAttribute('type', 'submit')    
         }
     }else{
